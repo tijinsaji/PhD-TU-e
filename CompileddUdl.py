4e4b79ad4facfa62c56effa83dae57272bba974b
tijinsaji <68032174+tijinsaji@users.noreply.github.com>
2025-11-25 16:19:45 +0100

    Updated with integrate function

--- CompileddUdl.py
+++ CompileddUdl.py
@@ -1,11 +1,11 @@
 #usr/bin/python3
 
-# A Python script to obtain the excess chemical potential from the folders of the thermodynamic integration simulation setup in Brick-CFCMC
-
 import matplotlib
 import matplotlib.pyplot as plt
 import numpy as np
 import os
+from scipy.interpolate import UnivariateSpline
+from scipy.integrate import quad
 from matplotlib.ticker import (MultipleLocator, FormatStrFormatter,
                                AutoMinorLocator)
 
@@ -13,7 +13,28 @@ xminorLocator = AutoMinorLocator()
 y1minorLocator = AutoMinorLocator()
 y2minorLocator = AutoMinorLocator()
 
-kJ_mol_Kelvin = 120
+
+def integrate(x, y, a, b):
+    """
+    x - independent variable of the data set
+    y - dependent variable of the data set
+    a - lower limit of integration (usually 0)
+    b - upper limit of integration (usually 1)
+    """
+    #Fit a spline
+    spline = UnivariateSpline(x, y, s=0)
+    
+    #Evaluate the spline
+    '''plt.plot(x, y, 'o', label='Data')
+    plt.plot(x, spline(x), '-', label='Cubic Spline')
+    plt.legend()
+    plt.show()'''
+    
+    #Integrate the spline
+    integral_value = spline.integral(a, b) 
+    return integral_value, spline(x)
+
+kJ_mol_Kelvin = 120	# 1 kJ/mol = 120 K
 
 root_folder = "./"
 
@@ -34,26 +55,26 @@ dUdl_list_sorted = sorted(dUdl_list)
 dUdl_array_sorted = np.array(dUdl_list_sorted)
 np.savetxt("Compiled_dU_dl.txt", dUdl_array_sorted, fmt=['%1.3f', ' %.4e', ' %.4e'], header='#Lambda dU/d\lambda[kJ/mol] Std.Err_dU/d\lambda [kJ/mol]')
 
-print(f"Integrated answer is {np.trapz(dUdl_array_sorted[:,1],dUdl_array_sorted[:,0])} kJ/mol")
+integrate_value , spline_fit = integrate(dUdl_array_sorted[:,0],dUdl_array_sorted[:,1], 0 , 1)
+
+#print(f"Integrated answer is {np.trapz(dUdl_array_sorted[:,1],dUdl_array_sorted[:,0])} kJ/mol")
+print(f"Integrated answer is {integrate_value:.3f} kJ/mol")
 
 ### Plotting ###
 fig, axs = plt.subplots(nrows=1, ncols=1, sharex=True, figsize=(6,4.2))
 fig.subplots_adjust(hspace=0.1)
 
-axs.plot(dUdl_array_sorted[:,0], dUdl_array_sorted[:,1],color='blue', linestyle='solid', linewidth=2, marker='s', markersize=4, label='NO$_{2}^{-}$ + H$_{3}$O$^{+}$')
-#axs.plot(dUdl_array_sorted[:,0], dUdl_array_sorted[:,2],color='red', linestyle='solid', linewidth=2, marker='o', markersize=4, label='Column 2')
+axs.plot(dUdl_array_sorted[:,0], dUdl_array_sorted[:,1],color='blue', linestyle='solid', linewidth=2, marker='s', markersize=4, label='NO$_{3}^{-}$ + H$_{3}$O$^{+}$')
+axs.plot(dUdl_array_sorted[:,0], spline_fit, linestyle='solid', color='darkorange', linewidth=2, label = 'Cubic fit')
 
 axs.grid(True)
 axs.grid(which='both')
 axs.grid(which='minor', alpha=0.1)
 axs.grid(which='major', alpha=0.5)
-axs.set_ylabel('$\langle \\frac{\partial U}{\partial \lambda} \\rangle$ / [kJ/mol]', fontsize=11)
-axs.set_xlabel('$\lambda$',  fontsize=11)
-#axs.set_ylim(-130,130)
-#axs.set_ylim(-5,20)
-#axs.set_xlim(1.8,4)
+axs.set_ylabel('$\langle \\frac{\partial U}{\partial \lambda} \\rangle$ / [kJ/mol]', fontsize=14)
+axs.set_xlabel('$\lambda$',  fontsize=14)
 legend = axs.legend(loc='lower left', shadow=False, fontsize=10, frameon=False)
 
 fig.tight_layout()
-fig.savefig('dUdl_NO2n_H3Op.pdf')
-fig.savefig('dUdl_NO2n_H3Op.svg')
+fig.savefig('dUdl_NO3n_H3Op_with_CubicFit.pdf')
+fig.savefig('dUdl_NO3n_H3Op_with_CubicFit.svg')
